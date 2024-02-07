34a966f6820a6ee8cc321ae3fa185a254863ae91
James Hart <jhart44@ut.utm.edu>
2024-02-06 21:09:11 -0600

    Added most functionality. This would be almost concidered at an alpha stage.

--- Vector.cpp
+++ Vector.cpp
@@ -3,33 +3,80 @@
 
 using namespace NonSTD;
 
+Vector::Vector(const Vector &other)
+{
+  vec_size = other.vec_size;
+  vec_capacity = other.vec_capacity;
+  vec_ptr = new int[vec_capacity];
+  for(int i = 0; i < vec_capacity; i++)
+    {
+      vec_ptr[i] = other.vec_ptr[i];
+    }
+}
+
 Vector::Vector()
 {
-  int vec_size = 0;
-  int vec_capcity = 0;
-  int *vec_ptr = NULL;
+  vec_size = 0;
+  vec_capacity = 0;
+  vec_ptr = new int[vec_capacity];
 }
 
 Vector::~Vector()
 {
-  delete vec_ptr;
+  delete[] vec_ptr;
+  vec_capacity = 0, vec_size = 0;
 }
 
 int Vector::size()
 {
-  //TODO: RETURNS THE CURRENT NUMBER OF ELEMENTS IN THE VECTOR.
-  
-  
+  //TODO: RETURNS THE CURRENT NUMBER OF ELEMENTS IN THE VECTOR. Completed: 02/05/24 4:09PM!
+
+  return vec_size;
 }
 
 int Vector::capacity()
 {
-  //TODO: RETURNS THE ALLOCATED STORAGE CAPCITY OF THE VECTOR.
+  //TODO: RETURNS THE ALLOCATED STORAGE CAPCITY OF THE VECTOR. Completed 02/05/24 4:09PM!
+  return vec_capacity;
 }
 
 
-//Vector& operator=(const Vector &other)
-//{
-//
-  //}
 
+//Vector& Vector::operator=(const Vector &other)
+//{
+//DEEP COPY
+//}
+void Vector::reserve(int n)
+{
+  //TODO: Requests that the vector capacity be resized at least enough to contain n elements.
+
+  if (vec_capacity < n)
+    {
+      int *temp = new int[n];
+      vec_capacity = n;
+      //std::cout << vec_capacity << std::endl;
+      //std::cout << "temp:    " <<  temp[1] << std::endl;
+      //std::cout << "vec_ptr: " << vec_ptr[1] << std::endl;
+      for(int i = 0; i < vec_capacity; i++)
+      {
+        temp[i] = vec_ptr[i]; 
+      }
+      delete[] vec_ptr;
+      vec_ptr = temp;
+    }
+}
+
+int& Vector::operator[](unsigned int index)
+{
+  //Returns a reference to the element at position index.
+  return vec_ptr[index];
+}
+
+void Vector::push_back(int element)
+{
+  if(vec_size + 1 >= vec_capacity)
+    {
+      reserve(vec_capacity+1);
+    }
+  vec_ptr[vec_size + 1] = element;
+}
--- main.cpp
+++ main.cpp
@@ -5,8 +5,39 @@ using namespace std;
 
 int main()
 {
+  NonSTD::Vector object;
 
 
+  char a = 0;
+  while (a != 'q')
+    {
+      cout << "Enter a number for an action." << endl << endl;
+
+      cout << "1: object.size" << endl;
+      cout << "2: object.reserve(10)" << endl;
+      cout << "q: To Exit." << endl;
+      cout << "Number: ";
+
+      cin >> a;
+
+      if(a == '1')
+	{
+	  cout << object.size() << endl;
+	  cout << "Size Test Complete" << endl;
+	}
+      else if (a == '2')
+	{
+	  object.reserve(10);
+	  cout << "Reserve Test Complete" << endl;
+	}
+      else
+	{
+	  cout << "invalid entry." << endl;
+	}
+    }
+  
+  cout << "Everything is working fine" << endl;
+  
   
   return 0;
 }
